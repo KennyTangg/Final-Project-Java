77f3e5987d735d849fe969ec14f2cea909b8376e
Kenny T <kenny.t4ng@gmail.com>
2025-04-21 09:45:46 +0700

    Initial commit

---
+++ src/main/java/com/contactsmanager/model/Contact.java
@@ -0,0 +1,9 @@
+package com.contactsmanager.model;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    // Constructor, getters, setters, and toString() implementations
+}

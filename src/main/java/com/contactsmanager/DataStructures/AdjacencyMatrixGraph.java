9886f1baf9a5b9d649562d93bc8a970760439e2e
frost-drago <tiffany.play28@gmail.com>
2025-05-09 12:32:21 +0700

    Modified Adjacency Matrix Graph to fit interface

--- src/main/java/com/contactsmanager/DataStructures/AdjacencyMatrixGraph.java
+++ src/main/java/com/contactsmanager/DataStructures/AdjacencyMatrixGraph.java
@@ -1,22 +1,179 @@
-package com.contactsmanager.DataStructures;
-import java.util.LinkedList;
+// Unlike adjacency list, this one has a fixed limit/ capacity.
 
-public class AdjacencyMatrixGraph {
-    int rows = 6;
-    int columns = 6;
-    int[][] matrix = new int[rows][columns];
+package com.contactsmanager.DataStructures;
+
+import com.contactsmanager.interfaces.ContactsManager;
+import com.contactsmanager.model.Contact;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class AdjacencyMatrixGraph implements ContactsManager{
+    int size;
+    int maxSize;
+    byte[][] matrix; // Where the connections are stored
+    Contact[] contactsBook; // Where the contact information are stored
     boolean directed;
 
-    // ADD CONNECTION
-    void addEdge(int a, int b) {
-        matrix[a][b] = 1;
-        if (!directed) {
-            matrix[b][a] = 1;
+    public AdjacencyMatrixGraph(int maxSize) {
+        this.size = 0;
+        this.maxSize = maxSize;
+        matrix = new byte[maxSize][maxSize];
+        contactsBook = new Contact[maxSize];
+        directed = false;
+    }
+
+    public AdjacencyMatrixGraph(int maxSize, boolean directed) {
+        this.size = 0;
+        this.maxSize = maxSize;
+        matrix = new byte[maxSize][maxSize];
+        contactsBook = new Contact[maxSize];
+        this.directed = directed;
+    }
+
+    // SEARCH A CONTACT -OK
+    @Override
+    public Contact searchContact(String name) {
+        for (Contact contact : contactsBook) {
+            if (contact.getName().equals(name)) {
+                return contact;
+            }
+        }
+        System.out.println("Contact not found: " + name);
+        return null;
+    }
+
+    // INTERNALLY SEARCH CONTACT BY INDEX
+    private int searchIndexOfContact(String name) {
+        int i = 0;
+        for (Contact contact : contactsBook) {
+            if (contact.getName().equals(name)) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    // INTERNALLY SEARCH BLANK SPACE IN CONTACT LIST
+    private int searchIndexOfFree() {
+        if (size == maxSize) {
+            System.out.println("No space left in contacts book.");
+            return -1;
+        }
+        int i = 0;
+        for (Contact contact : contactsBook) {
+            if (contact == null) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    // ADD A NODE -OK
+    @Override
+    public void addContact(Contact contact) {
+        int free = searchIndexOfFree();
+        if (free == -1) {
+            return;
+        }
+        contactsBook[free] = contact;
+        size++;
+    }
+
+    // DELETE NODE -OK
+    @Override
+    public void deleteContact(String name) {
+        int target = searchIndexOfContact(name);
+
+        for (int i = 0; i < maxSize; i++){ // Delete connections
+            matrix[i][target] = 0;
+        }
+        for (int j = 0; j < maxSize; j++){
+            matrix[target][j] = 0;
+        }
+
+        contactsBook[target] = null; // Delete contact info
+        size--;
+    }
+
+    // UPDATE CONTACT -OK
+    @Override
+    public void updateContact(Contact contact, String newName, int newStudentId) {
+        String name = contact.getName();
+        int target = searchIndexOfContact(name);
+        Contact newContact = new Contact(newName, newStudentId); // Make new contact to replace old one
+        contactsBook[target] = newContact;
+    }
+
+    // LIST ALL CONTACTS -OK
+    @Override
+    public List<Contact> listAllContacts() {
+        return List.of(contactsBook);
+    }
+
+    // ADD CONNECTION -OK
+    @Override
+    public void addConnection(String contact1, String contact2) {
+        int yIndex = searchIndexOfContact(contact1); // Find index of both strings for the matrix
+        int xIndex = searchIndexOfContact(contact2);
+        matrix[yIndex][xIndex] = 1;
+        if (!directed) { // Undirected graph
+            matrix[xIndex][yIndex] = 1;
         }
     }
 
+    // DELETE CONNECTION -OK
+    @Override
+    public void removeConnection(String contact1, String contact2) {
+        int yIndex = searchIndexOfContact(contact1); // Find index of both strings for the matrix
+        int xIndex = searchIndexOfContact(contact2);
+        matrix[yIndex][xIndex] = 0;
+        if (!directed) { // Undirected graph
+            matrix[xIndex][yIndex] = 0;
+        }
+    }
+
+    // SUGGEST CONTACTS -OK
+    @Override
+    public List<Contact> suggestContacts(String contact) {
+        LinkedList<Integer> directConnectionsIndex = new LinkedList<>();
+        HashSet<Integer> recommendedContactsIndex = new HashSet<>();
+        LinkedList<Contact> recommendedContacts = new LinkedList<>();
+        int target = searchIndexOfContact(contact);
+
+        for (int i = 0; i < maxSize; i++) { // Find person's friends (by index)
+            if (matrix[target][i] == 1) {
+                directConnectionsIndex.add(i);
+            }
+        }
+        if (directConnectionsIndex.isEmpty()) {
+            System.out.println("Unable to suggest contacts from not knowing anyone.");
+            return null;
+        }
+        for (int directConnection : directConnectionsIndex) { // Find friend's friends (by index)
+            for (int i = 0; i < maxSize; i++) {
+                if ((matrix[directConnection][i] == 1) && (i != target)) {
+                    recommendedContactsIndex.add(i);
+                }
+            }
+        }
+        for (int suggested : recommendedContactsIndex) { // Change indexes to contact
+            recommendedContacts.add(contactsBook[suggested]);
+        }
+
+        return recommendedContacts;
+    }
+
+
+    // LEGACY (modify or delete)
+
     // TRAVERSAL DEPTH FIRST SEARCH
     /* There could be multiple paths from the start cell to the destination cell*/
+    /*
     boolean dfsFindPath(int[][] matrix, int sx, int sy, int dx, int dy, int passable) {
         boolean[][] visited = new boolean[matrix.length][matrix[0].length];
         dfs(matrix, sx, sy, visited, passable); // Call dfs()
@@ -106,6 +263,7 @@ public class AdjacencyMatrixGraph {
             queue.add(cell); // Enqueue cell
         }
     } // Time O(1), Space O(1)
+     */
 
     /*Both DFS and BFS for a matrix have a time and space complexity of O(m*n).*/
 }
